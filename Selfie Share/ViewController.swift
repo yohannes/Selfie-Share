98831ed4364041d3481ef822e4dd950052e114df
Yohannes Wijaya <yohannes.wijaya@gmail.com>
2015-10-08 15:30:52 +0700

    Setup base UI elements, properties, and delegate methods for importing photos.

--- Selfie Share/ViewController.swift
+++ Selfie Share/ViewController.swift
@@ -8,11 +8,23 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    // MARK: - Stored Properties
+    
+    var images = [UIImage]()
+    
+    // MARK: - IBOutlet Properties
+    
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    // MARK: - Methods Override
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        self.title = "Selfie Share"
+        self.navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .Camera, target: self, action: "importPicture")
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +32,50 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    // MARK: - Delegate Methods
+    
+    func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return self.images.count ?? 0
+    }
+    
+    func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        let collectionViewCell = collectionView.dequeueReusableCellWithReuseIdentifier("ImageViewReusableIdentifier", forIndexPath: indexPath)
+        
+        if let imageView = collectionViewCell.viewWithTag(1000) as? UIImageView {
+            imageView.image = self.images[indexPath.item]
+        }
+        
+        return collectionViewCell
+    }
+    
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
+        var newImage: UIImage!
+        
+        if let possibleImage = info["UIImagePickerControllerEditedImage"] as? UIImage {
+            newImage = possibleImage
+        }
+        else if let possibleImage = info["UIImagePickerControllerOriginalImage"] as? UIImage {
+            newImage = possibleImage
+        }
+        else { return }
+        
+        self.dismissViewControllerAnimated(true, completion: nil)
+        
+        self.images.insert(newImage, atIndex: 0)
+        self.collectionView.reloadData()
+    }
+    
+    func imagePickerControllerDidCancel(picker: UIImagePickerController) {
+        self.dismissViewControllerAnimated(true, completion: nil)
+    }
 
+    // MARK: - Local Methods
+    
+    func importPicture() {
+        let imagePickerController = UIImagePickerController()
+        imagePickerController.allowsEditing = true
+        imagePickerController.delegate = self
+        self.presentViewController(imagePickerController, animated: true, completion: nil)
+    }
 }
 
