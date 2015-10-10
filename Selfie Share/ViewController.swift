c685f2fd3fe4754181ff121d44afdd31910ec9fe
Yohannes Wijaya <yohannes.wijaya@gmail.com>
2015-10-10 14:51:51 +0700

    Implemented the required methods to conform to MCSessionDelegate & MCBrowserViewControllerDelegate.

--- Selfie Share/ViewController.swift
+++ Selfie Share/ViewController.swift
@@ -47,10 +47,15 @@ class ViewController: UIViewController, UICollectionViewDataSource, UICollection
 
     // MARK: - Delegate Methods
     
+    func browserViewControllerDidFinish(browserViewController: MCBrowserViewController) {
+        self.dismissViewControllerAnimated(true, completion: nil)
+    }
+    func browserViewControllerWasCancelled(browserViewController: MCBrowserViewController) {
+        self.dismissViewControllerAnimated(true, completion: nil)
+    }
     func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return self.images.count ?? 0
     }
-    
     func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
         let collectionViewCell = collectionView.dequeueReusableCellWithReuseIdentifier("ImageViewReusableIdentifier", forIndexPath: indexPath)
         
@@ -76,11 +81,47 @@ class ViewController: UIViewController, UICollectionViewDataSource, UICollection
         
         self.images.insert(newImage, atIndex: 0)
         self.collectionView.reloadData()
+        
+        // 1. check if there's any peer to connect to
+        if self.mcSession.connectedPeers.count > 0 {
+            // 2. convert new image to NSData
+            if let imageData = UIImagePNGRepresentation(newImage) {
+                // 3. send it to all peers making sure it gets delivered
+                do {
+                    try self.mcSession.sendData(imageData, toPeers: self.mcSession.connectedPeers, withMode: MCSessionSendDataMode.Reliable)
+                }
+                catch let error as NSError {
+                    let alertController = UIAlertController(title: "Send error", message: error.localizedDescription, preferredStyle: .Alert)
+                    let alertAction = UIAlertAction(title: "OK", style: .Default, handler: nil)
+                    alertController.addAction(alertAction)
+                    self.presentViewController(alertController, animated: true, completion: nil)
+                }
+            }
+        }
+        
     }
-    
     func imagePickerControllerDidCancel(picker: UIImagePickerController) {
         self.dismissViewControllerAnimated(true, completion: nil)
     }
+    
+    func session(session: MCSession, peer peerID: MCPeerID, didChangeState state: MCSessionState) {
+        switch state {
+        case MCSessionState.Connected: print("Connected: \(peerID.displayName)")
+        case MCSessionState.Connecting: print("Connecting: \(peerID.displayName)")
+        case MCSessionState.NotConnected: print("Not connected: \(peerID.displayName)")
+        }
+    }
+    func session(session: MCSession, didReceiveData data: NSData, fromPeer peerID: MCPeerID) {
+        if let image = UIImage(data: data) {
+            dispatch_async(dispatch_get_main_queue(), { [unowned self] () -> Void in
+                self.images.insert(image, atIndex: 0)
+                self.collectionView.reloadData()
+            })
+        }
+    }
+    func session(session: MCSession, didReceiveStream stream: NSInputStream, withName streamName: String, fromPeer peerID: MCPeerID) {}
+    func session(session: MCSession, didStartReceivingResourceWithName resourceName: String, fromPeer peerID: MCPeerID, withProgress progress: NSProgress) {}
+    func session(session: MCSession, didFinishReceivingResourceWithName resourceName: String, fromPeer peerID: MCPeerID, atURL localURL: NSURL, withError error: NSError?) {}
 
     // MARK: - Local Methods
     
